59a4f88909baeda6d03c909b6d86810608319f15
Ardian Pramudya Alphita <ardianpramudya81@gmail.com>
2023-03-22 19:32:49 +0700

    Initial Commit

---
+++ SPTSampleSwiftUI/SPTSampleSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SPTSampleSwiftUIApp.swift
+//  SPTSampleSwiftUI
+//
+//  Created by Ardian Pramudya Alphita on 22/03/23.
+//
+
+import SwiftUI
+
+@main
+struct SPTSampleSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
